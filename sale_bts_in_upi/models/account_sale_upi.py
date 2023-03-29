f9eb31e24dfc2736fe7debcc9c084c86247e8230
BitsUp Technologies <bitsuptechnologies@gmail.com>
2023-03-30 01:50:58 +0530

    sale upi error correction 1.3

--- sale_bts_in_upi/models/account_sale_upi.py
+++ sale_bts_in_upi/models/account_sale_upi.py
@@ -9,40 +9,13 @@ from odoo.tools.image import image_data_uri
 class SalesOrderUpi(models.Model):
     _inherit = "sale.order"
 
-    def _generate_qr_code(self, silent_errors=False):
+    def _salegenerate_qr_code(self, silent_errors=False):
         self.ensure_one()
         if self.company_id.country_code == 'IN':
-            payment_url = 'upi://pay?pa=%s&pn=%s&am=%s&tr=%s&tn=%s' % (
+            qr_code_url = 'upi://pay?pa=%s&pn=%s&am=%s&tr=%s&tn=%s' % (
                 self.company_id.l10n_in_upi_id,
                 self.company_id.name,)
             barcode = self.env['ir.actions.report'].barcode(barcode_type="QR", value=payment_url, width=120, height=120)
             return image_data_uri(base64.b64encode(barcode))
-        return super()._generate_qr_code(silent_errors)
-
- # === Payment fields === #
-    payment_reference = fields.Char(
-        string='Payment Reference',
-        index='trigram',
-        copy=False,
-        help="The payment reference to set on journal items.",
-        tracking=True,
-    )
-    display_qr_code = fields.Boolean(
-        string="Display QR-code",
-        compute='_compute_saledisplay_qr_code',
-    )
-    qr_code_method = fields.Selection(
-        string="Payment QR-code", copy=False,
-        selection=lambda self: self.env['res.partner.bank'].get_available_qr_methods_in_sequence(),
-        help="Type of QR-code to be generated for the payment of this invoice, "
-             "when printing it. If left blank, the first available and usable method "
-             "will be used.",
-    )
-
-    def _compute_saledisplay_qr_code(self):
-        for record in self:
-            record.display_qr_code = (
-                record.state in ('draft', 'sent')
-                and record.company_id.qr_code
-            )
+        return super()._salegenerate_qr_code(silent_errors)
 
