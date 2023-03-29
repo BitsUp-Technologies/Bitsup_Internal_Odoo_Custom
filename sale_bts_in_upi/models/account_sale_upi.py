5c16c3b569ad2ed9324b21ccea6121dba221c2c5
BitsUp Technologies <bitsuptechnologies@gmail.com>
2023-03-30 01:01:50 +0530

    saleupiupdate

--- sale_bts_in_upi/models/account_sale_upi.py
+++ sale_bts_in_upi/models/account_sale_upi.py
@@ -21,3 +21,32 @@ class SalesOrderUpi(models.Model):
             barcode = self.env['ir.actions.report'].barcode(barcode_type="QR", value=payment_url, width=120, height=120)
             return image_data_uri(base64.b64encode(barcode))
         return super()._generate_qr_code(silent_errors)
+
+ # === Payment fields === #
+    payment_reference = fields.Char(
+        string='Payment Reference',
+        index='trigram',
+        copy=False,
+        help="The payment reference to set on journal items.",
+        tracking=True,
+    )
+    display_qr_code = fields.Boolean(
+        string="Display QR-code",
+        compute='_compute_saledisplay_qr_code',
+    )
+    qr_code_method = fields.Selection(
+        string="Payment QR-code", copy=False,
+        selection=lambda self: self.env['res.partner.bank'].get_available_qr_methods_in_sequence(),
+        help="Type of QR-code to be generated for the payment of this invoice, "
+             "when printing it. If left blank, the first available and usable method "
+             "will be used.",
+    )
+
+  @api.depends('company_id')
+    def _compute_saledisplay_qr_code(self):
+        for record in self:
+            record.display_qr_code = (
+                record.state in ('draft', 'sent')
+                and record.company_id.qr_code
+            )
+
